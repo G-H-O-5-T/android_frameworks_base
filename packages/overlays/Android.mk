c384156f7a9966328f2b1c688f109cb2bd0a0c06
Lup Gabriel <gwolf2u.website@gmail.com>
2020-02-03 18:47:54 +0200

    Add GrandHotel and Redressed fonts [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I93466445ea7ca7b9778cfda74ddb19bd12008523
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -60,6 +60,8 @@ LOCAL_REQUIRED_MODULES := \
 	FontRoadRageSourceOverlay \
 	FontCoconSourceOverlay \
 	FontQuandoSourceOverlay \
+	FontGrandHotelSourceOverlay \
+	FontRedressedSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
